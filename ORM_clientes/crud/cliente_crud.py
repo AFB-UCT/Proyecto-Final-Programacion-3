02d9870f64a8c83964bc34591b34d70245545623
AFB-UCT <agustin.flores2024@alu.uct.cl>
2025-11-10 22:16:36 -0300

    modified:   ORM_clientes/crud/cliente_crud.py 	modified:   ORM_clientes/crud/ingrediente_crud.py 	modified:   ORM_clientes/crud/menu_crud.py 	modified:   ORM_clientes/crud/pedido_crud.py 	modified:   ORM_clientes/database.py 	modified:   ORM_clientes/main.py 	modified:   ORM_clientes/models.py 	modified:   PixelFood.db 	deleted:    Restaurant.db

---
+++ ORM_clientes/crud/cliente_crud.py
@@ -0,0 +1,44 @@
+from sqlalchemy.orm import Session
+
+from models import Cliente
+
+def crear_cliente(db: Session, nombre: str):
+
+    nuevo_cliente = Cliente(nombre=nombre)
+
+    db.add(nuevo_cliente)
+
+    db.commit()
+
+    db.refresh(nuevo_cliente)
+
+    return(nuevo_cliente)
+
+def obtener_cliente(db: Session, cliente_id: int):
+
+    return db.query(Cliente).filter(Cliente.id==cliente_id).first()
+
+def obtener_clientes(db: Session):
+    return db.query(Cliente).all()
+
+def eliminar_cliente(db: Session, cliente_id:int):
+    cliente = db.query(Cliente).filter(Cliente.id == cliente_id).first()
+
+    if cliente:
+        db.delete(cliente)
+
+        db.commit()
+
+    return cliente
+
+def actualizar_cliente(db: Session, cliente_id:int, nombre:str):
+
+    cliente = db.query(Cliente).filter(Cliente.id == cliente_id).first()
+
+    if cliente:
+        cliente.nombre = nombre
+
+        db.commit()
+
+        db.refresh(cliente)
+    return cliente
--- ORM_clientes/main.py
+++ ORM_clientes/main.py
@@ -1,6 +1,8 @@
 from sqlalchemy import inspect
-from database import Base, Engine
+from database import Base, Engine, session
 from models import Cliente, Ingredientes, Pedido, Menus
+from crud.cliente_crud import *
+from crud.pedido_crud import *
 
 
 
@@ -10,5 +12,7 @@ if __name__ == '__main__':
     Base.metadata.create_all(Engine)
 
 
-inspector = inspect(Engine)
-print(inspector.get_table_names())
+    inspector = inspect(Engine)
+    print(inspector.get_table_names())
+    cliente1=crear_cliente(session, nombre="Carlos")
+    plato1=crear_pedido(session, nombre="Hamburgueja",cantidad=666, precio=1590, cliente_id=cliente1.id)
